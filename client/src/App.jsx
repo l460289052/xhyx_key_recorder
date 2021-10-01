57be4cd8fbdd2b422083afebe1318e3a38dfd025
l4602 <l460289052@163.com>
2021-10-01 21:38:19 +0800

    change trigger

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css'
 import './App.css';
 import { Button, Layout, Menu, Space, Switch } from 'antd'
-import { AreaChartOutlined, InfoCircleOutlined, SearchOutlined } from '@ant-design/icons';
+import { AreaChartOutlined, InfoCircleOutlined, MenuFoldOutlined, MenuUnfoldOutlined, SearchOutlined } from '@ant-design/icons';
 import { PageSearch, PageStatistics } from './pages/pages'
 import axios from 'axios';
 
 const { Header, Footer, Content, Sider } = Layout;
-const Slider = Sider;
 
 
 var Pages = {
@@ -27,7 +26,6 @@ function Page(props) {
     case Pages.Statistics:
       return <PageStatistics />
     case Pages.Analysis:
-      console.log(222)
       return <p>Analysis</p>;
 
     case Pages.Search:
@@ -38,17 +36,16 @@ function Page(props) {
 
 
 function App() {
-  var [collapsed, setCollapsed] = useState(false);
+  var [collapsed, setCollapsed] = useState(true);
   var [running, setRunning] = useState(false)
   var [page, setPage] = useState(Pages.Search)
 
   useEffect(() => axios.get("/api/get_hook_state")
     .then(rep => setRunning(rep.data.running))
     , [])
-
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Slider collapsible collapsed={collapsed} onCollapse={(collapsed, type) => setCollapsed(collapsed)}>
+      <Sider collapsible collapsedWidth="0" trigger={null} collapsed={collapsed}>
         <div className="block">
         </div>
         <div style={{ textAlign: 'center' }}>
@@ -69,9 +66,15 @@ function App() {
           <Menu.Item key={Pages.Statistics} icon={<InfoCircleOutlined />} >{ShownName[Pages.Statistics]}</Menu.Item>
           <Menu.Item key={Pages.Analysis} icon={<AreaChartOutlined />}>{ShownName[Pages.Analysis]}</Menu.Item>
         </Menu>
-      </Slider>
+      </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: (e) => setCollapsed(!collapsed)
+          }
+          )}
+        </Header>
         <Content style={{ margin: '0 16px' }}>
           <p>{ShownName[page]}</p>
           <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
