07c8ed64908833438d206d57c5ad7d3435331488
l4602 <l460289052@163.com>
2021-12-30 10:02:49 +0800

    default start

--- key_recorder.py
+++ key_recorder.py
@@ -8,7 +8,7 @@ ctypes.windll.user32.SetProcessDPIAware()
 
 import PySimpleGUI as sg
 
-layout = [[sg.Text("", auto_size_text=True, key="-pbyb-"), sg.Checkbox("跟随", enable_events=True, key="Follow"), sg.Button("开始", key="Toggle")],
+layout = [[sg.Text("", auto_size_text=True, key="-pbyb-")],
           [sg.Text("", auto_size_text=True, key="-hjzi-"), sg.Button("关闭", key="Cancel")]]
 win = sg.Window("key optimizer", layout, no_titlebar=True,
                 keep_on_top=True, alpha_channel=.7, grab_anywhere=True, font="微软雅黑")
@@ -17,7 +17,6 @@ remover = None
 word_getter = None
 code = deque()
 now_word = deque()
-win_pos = word_handler.win_position.WinPosition()
 
 
 table = word_handler.code_table.get_table()
@@ -45,6 +44,10 @@ def get_word(win: sg.Window):
         logging.getLogger("exception").exception(e)
 
 
+threading.Thread(target=get_word, args=(
+    win,), daemon=True).start()
+remover = keyboard.hook(word_handler.record.record)
+
 while True:
     event, values = win.read()
     try:
@@ -55,27 +58,9 @@ while True:
                     remover()
                     word_handler.record.queue.put("Stop")
                 break
-            case "Toggle":
-                button: sg.Button = win["Toggle"]
-                if remover is not None:
-                    button.update("开始")
-                    remover()
-                    remover = None
-                    word_handler.record.queue.put("Stop")
-                else:
-                    button.update("停止")
-                    threading.Thread(target=get_word, args=(
-                        win,), daemon=True).start()
-                    remover = keyboard.hook(word_handler.record.record)
-
-            case "Follow":
-                # cannot update in here, because there is not caret when toggling checkbox
-                if not values["Follow"]:
-                    win_pos.clear()
 
             case "-MOVE-":
                 win.move(*word_handler.get_win.get_cursor_position())
-                win_pos.clear()
 
             case "-WORD-":
                 word: word_handler.code_table.InputWord = values["-WORD-"]
@@ -96,11 +81,6 @@ while True:
                         hjzi: sg.Text = win["-hjzi-"]
                         hjzi.update(ret.word)
 
-                if values["Follow"]:
-                    pos = win_pos.get_pos(win.current_location())
-                    if not pos:
-                        continue
-                    win.move(*pos)
     except Exception as e:
         import logging
         logging.getLogger("exception").exception(e)
