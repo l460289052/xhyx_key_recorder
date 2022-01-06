054863b55e846a9efabcb2d72ce06ef0f7773820
l4602 <l460289052@163.com>
2022-01-06 09:47:39 +0800

    state machine

--- word_handler/record.py
+++ word_handler/record.py
@@ -1,26 +1,44 @@
-from datetime import datetime
 from multiprocessing import Queue
+from typing import Any, List, Optional, Tuple, Union
 
 import keyboard
 
 from .get_win import is_chinese
-
 
 hotkeys = {'ctrl', 'alt'}
 pressed_hotkeys = set()
 
 queue = Queue()
 
-last_key = None
+
+class StateMachine:
+    def __init__(self) -> None:
+        self.root = {}
+        self.state = self.root
+
+    def add_path(self, path: List[Tuple[Any]], key: str):  # 需要在这里建立失匹配函数了……
+        current = self.root
+        for p in path:
+            assert p != "ret"
+            current = current.setdefault(p, {})
+        current["ret"] = key
+
+    def match(self, event: Tuple[Any]) -> Optional[str]:
+        self.state = self.state.get(event, self.root.get(event, self.root))
+        return self.state.get("ret", None)
+
+
+sm = StateMachine()
+sm.add_path([("ctrl", keyboard.KEY_DOWN),
+            ("ctrl", keyboard.KEY_UP)] * 2, "double ctrl")
 
 
 def record(e: keyboard.KeyboardEvent):
-    global last_key
     try:
-        if e.event_type == keyboard.KEY_DOWN:
-            if last_key == e.name == "ctrl":
+        match sm.match((e.name, e.event_type)):
+            case "double ctrl":
                 queue.put("-MOVE-")
-            last_key = e.name
+
         # if not is_chinese():
         #     return pressed_hotkeys.clear()
 
